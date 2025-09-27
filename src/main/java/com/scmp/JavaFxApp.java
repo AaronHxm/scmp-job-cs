bac5a666c4592d73585ef43f8dd3d906d1855c77
Hu Xianming <78032724@qq.com>
2025-09-27 10:05:32 +0800

    增加日志

--- src/main/java/com/scmp/JavaFxApp.java
+++ src/main/java/com/scmp/JavaFxApp.java
@@ -2,9 +2,22 @@ package com.scmp;
 
 import com.scmp.ui.LoginUI;
 import javafx.application.Application;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class JavaFxApp {
     public static void main(String[] args) {
-        Application.launch(LoginUI.class, args);
+        try {
+            Application.launch(LoginUI.class, args);
+        } catch (Exception e) {
+            String logPath = System.getProperty("user.home") + "/app_error.log";
+            try (PrintWriter writer = new PrintWriter(new FileWriter(logPath, true))) {
+                e.printStackTrace(writer);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            System.err.println("应用程序启动失败，错误日志已保存至: " + logPath);
+        }
     }
 }
